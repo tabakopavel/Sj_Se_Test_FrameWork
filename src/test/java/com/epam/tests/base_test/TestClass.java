ba9c275a815eb1b9d5264eba1db6b408af4158ea
Pavel_Tabako <Pavel_Tabako@epam.com>
2017-10-03 16:03:38 +0300

    test edit

--- src/test/java/com/epam/tests/base_test/TestClass.java
+++ src/test/java/com/epam/tests/base_test/TestClass.java
@@ -2,6 +2,7 @@ package com.epam.tests.base_test;
 
 import com.epam.framework.core.driver.Driver;
 import com.epam.tests.base_test.BaseTest;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -17,5 +18,9 @@ public class TestClass extends BaseTest {
     public void test(){
        WebDriver driver = Driver.getDriver();
         driver.get("https://www.onliner.by/");
+
+        if(driver.findElements(By.xpath(".//*[@id='container']/div/div[2]/header/div[2]/div/div[1]/a/img")).size()!=0){
+            System.out.println("true");
+        }
     }
 }
